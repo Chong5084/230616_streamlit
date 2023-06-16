238deea42b07c3fd475be011ca551f74112cdb3a
Chong5084 <dmschd9647@gmail.com>
2023-06-16 12:31:07 +0900

    ~컴포넌트

--- app.py
+++ app.py
@@ -301,9 +301,61 @@ with col2: # col2을 기준으로     streamit을 써주겠다
     # 블록(:)을 열면 -> 이 안에서는 streamit 실행 시 cols2 에 종속
     st.write("오른 쪽")
 
+# st.tabs(["김치찌개", "된장찌개", "찌개"])
+# tab1, tab2, tab3 = st.tabs(["김치찌개", "된장찌개", "찌개"]) # 위의 코드를 언팩킹
+
+# tabs = st.tabs(["김치찌개", "된장찌개", "로제마라어묵찌개"])
+tab_menus = ["김치찌개", "된장찌개", "로제마라어묵찌개"]
+tab1, tab2, tab3 = st.tabs(tab_menus)
+img1 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4MFBhxEu7tdtEmSpJ-DEzVl9si8NYOiYmbruRyOr7vYS8ZMLSpu60YsPo5WtmB5xg_F0&usqp=CAU"
+tab1.image(img1)
+with tab2:
+    img2 = "https://imagescdn.gettyimagesbank.com/500/201708/jv10940386.jpg"
+    st.image(img2)
+tab3.write("이런 건 없어요... 상상도 마라요")
+
+exp = st.expander("Surprise!!!", expanded=False)
+exp.image("https://i.namu.wiki/i/5lWwYGj-VC8ZqJxug7Exm5-7rHE97fdZui3DWEAjm0zdLiBCbcdw4mLyGhcbZ_KecZOQr4rtwNJSFs63Rsdd_Q.webp")
+# with exp: ...
+
+#입력
+st.title("입력")
+name = st.text_input("와타시노 나마에와")  # 변수로 받을 수 있음
+name2 = st.text_input("키미노 나마에와")  # 변수로 받을 수 있음
+# st.text_input("")
+# st.write(name)
+# st.write(name2)
+st.write(f"신랑 {name}과 신부 {name2}는...")
+# number = st.number_input("당신의 나이는?")
+age = st.number_input("당신의 나이는?", step=1)
+st.write(f"나의 나이는 {age}세")
+height = st.number_input("당신의 키는?", step=0.1)
+st.write(f"나의 키는 {height}cm")
+# https://docs.streamlit.io/library/api-reference/widgets
 
 
-
+st.divider()
+mode = st.checkbox("강사님 잔소리모드")  # bool (T/F)
+col1, col2, col3 = st.columns(3)
+r = col1.radio("잔소리 내용 선택", ["취업", "코딩", "지각"])
+s = col2.slider("잔소리 강도 선택", min_value=1, max_value=10)
+b = col3.selectbox("잔소리 말투 선택", ["친절하게", "반말", "모욕적"])
+if mode:
+    # r -> 취업, 코딩, 지각
+    format = None
+    if b == "친절하게":
+        format = lambda x: f"여러분~ {x}"
+    elif b == "반말":
+        format = lambda x: f"야! {x}"
+    elif b == "모욕적":
+        format = lambda x: f"XXXXXX! {x}"
+    if r == "취업":
+        for i in range(s):
+            st.write(format("8월에는 자소서 넣어야겠죠?")) # format("") #("")은 변수 x에 기입됨
+    elif r == "코딩":
+        st.write(format("저보다 파이썬 잘해요?"))
+    elif r == "지각":
+        st.write(format("9시랑 9시 1분은 다른 거에요."))
 
 
 
