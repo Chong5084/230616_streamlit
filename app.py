8182c73a6465a33392ca69c6903a3a028a7d9b2e
Chong5084 <dmschd9647@gmail.com>
2023-06-16 10:05:31 +0900

    오전

--- app.py
+++ app.py
@@ -5,11 +5,13 @@
 import streamlit as st  # streamlit -> import (가져오기) -> as (st 이름)
 # st라는 변수명으로 streamlit의 기능들을 사용하겠다
 
-# st. -> ctrl + space -> 다양한 기능(함수, 메소드)을 가지고 있다
+# st. -> ctrl + space(기능보기) -> 다양한 기능(함수, 메소드)을 가지고 있다
 
 st.title("나의 파이썬 웹 페이지")
 st.header("수업 8일차에 만들었어요")
 st.subheader("그래도 잘 만들었죠?")
 st.write("내가 만든 페이지, 너를 위해 구웠지 feat. Chong")
+
+st.video("https://www.youtube.com/watch?v=SaCheA6Njc4")
 
 # streamlit run app.py
