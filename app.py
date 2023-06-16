29a89afe052967d31b35eb4ccb1ae49aacdaa13d
Chong5084 <dmschd9647@gmail.com>
2023-06-16 10:03:54 +0900

    오전

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-# pip install streamlit
-# streamlit hello
+# $ pip install streamlit
+# $ streamlit hello # $ = 터미널에 쓰인다는 의미
 # ctrl + c : 실행 종료 (터미널)
 
 import streamlit as st  # streamlit -> import (가져오기) -> as (st 이름)
@@ -10,6 +10,6 @@ import streamlit as st  # streamlit -> import (가져오기) -> as (st 이름)
 st.title("나의 파이썬 웹 페이지")
 st.header("수업 8일차에 만들었어요")
 st.subheader("그래도 잘 만들었죠?")
-st.write("내가 만든 페이지, 너를 위해 구웠지")
+st.write("내가 만든 페이지, 너를 위해 구웠지 feat. Chong")
 
 # streamlit run app.py
